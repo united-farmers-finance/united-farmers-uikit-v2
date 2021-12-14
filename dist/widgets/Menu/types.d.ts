6ded468d80eb1352268451822aff039dfe0aaee6
Kwangwoo <bnc3049@gmail.com>
2021-12-13 21:14:50 -0700

    Add notification test3

---
+++ dist/widgets/Menu/types.d.ts
@@ -0,0 +1,50 @@
+import { HTMLAttributeAnchorTarget, ReactElement } from "react";
+import { Colors } from "../../theme/types";
+export interface Language {
+    code: string;
+    language: string;
+    locale: string;
+}
+export interface PushedProps {
+    isPushed: boolean;
+    pushNav: (isPushed: boolean) => void;
+}
+export interface NavTheme {
+    background: string;
+}
+export interface LinkStatus {
+    text: string;
+    color: keyof Colors;
+}
+export interface MenuSubEntry {
+    target?: HTMLAttributeAnchorTarget | undefined;
+    label: string;
+    href: string;
+    calloutClass?: string;
+    status?: LinkStatus;
+}
+export interface MenuEntry {
+    target?: HTMLAttributeAnchorTarget | undefined;
+    label: string;
+    icon: string;
+    items?: MenuSubEntry[];
+    href?: string;
+    calloutClass?: string;
+    initialOpenState?: boolean;
+    status?: LinkStatus;
+}
+export interface PanelProps {
+    isDark: boolean;
+    toggleTheme: (isDark: boolean) => void;
+    cakePriceUsd?: number;
+    grainPriceUsd?: number;
+    currentLang: string;
+    langs: Language[];
+    setLang: (lang: Language) => void;
+    links: Array<MenuEntry>;
+}
+export interface NavProps extends PanelProps {
+    notificationMenu?: ReactElement;
+    userMenu?: ReactElement;
+    globalMenu?: ReactElement;
+}
